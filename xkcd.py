eda4aaa8e49de9d00333ed70ab145c58624679dd
Swojit Mohapatra <neelmohapatra@gmail.com>
2017-01-22 08:39:18 -0800

    Only images as output and removed the random num_generator

--- xkcd.py
+++ xkcd.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python3
 import os, random
-rand_num = random.randint(1, 1788)
-os.system('curl -s https://xkcd.com/' + str(rand_num) + '/ > temp')
+os.system('curl -sL c.xkcd.com/random/comic/ > temp')
 link = os.popen("grep 'Image URL' temp").read().split()[-1]
 
 img = link.split('/')[-1]
-os.system('wget -nv ' + link + ' && ./imgcat ' + img)
+os.system('wget -q ' + link + ' && ./imgcat ' + img)
 os.system('rm temp ' + img)

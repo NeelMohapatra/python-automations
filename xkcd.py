b0cebdd784caff705fff34bf917eeea635d4d917
Swojit Mohapatra <neelmohapatra@gmail.com>
2017-01-23 23:22:56 -0800

    Removed useless import

--- xkcd.py
+++ xkcd.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # To display random comics from xkcd in iTerm2 and obviously you need iTerm2 for this
-import os, random
+import os
 os.system('curl -sL c.xkcd.com/random/comic/ > temp')
 link = os.popen("grep 'Image URL' temp").read().split()[-1]
 

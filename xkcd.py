88911f683b4dc79c3d246e60694ef8117398f3e9
Swojit Mohapatra <neelmohapatra@gmail.com>
2017-02-02 18:01:59 -0800

    Made xkcd random again

--- xkcd.py
+++ xkcd.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 # To display random comics from xkcd in iTerm2 and obviously you need iTerm2 for this
 
-import os, json, argparse, requests
+import os, json, argparse, requests, random
 
 page = json.loads(requests.get('http://xkcd.com/info.0.json').text)
 link = page['img']
+num = page['num']
+random_page = json.loads(requests.get('http://xkcd.com/' + str(random.randint(0, int(num))) + '/info.0.json').text)
+link = random_page['img']
 img = link.split('/')[-1]
 os.system('wget -q ' + link + ' && ./imgcat ' + img)
 os.system('rm ' + img)
